60a746aa07cf3c4ca16c60a4ba349685db33a7fe
Matthias Wagner <matzewagner@gmail.com>
2017-01-26 18:01:47 -0800

    updated answer links

--- source.js
+++ source.js
@@ -4,6 +4,7 @@ $(document).ready( () => {
     let tags = [];
     let headline = [];
     let answerLinks = [];
+    let answerLinksText = [];
     let sideLinks = [];
 
     const stopWords = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're"," weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've", "way", "out", "code", "work", "working", "try", "trying", "tried"];
@@ -49,31 +50,32 @@ $(document).ready( () => {
         $('body').find('.accepted-answer').find('.post-text').find('a').each(function(el) {
             let item = $(this).attr('href');
             arr.push(item);
+            answerLinksText.push($(this).text());
         });
         tempSet = new Set(arr);
         return Array.from(tempSet);
     }
-    function parseSideLinks () {
-        let arr = [];
-        $('body').find('.sidebar-linked').find('a').each(function(el) {
-            arr.push($(this).attr('href'));
-        });
-        tempSet = new Set(arr);
-        return Array.from(tempSet);
-    }
+    // function parseSideLinks () {
+    //     let arr = [];
+    //     $('body').find('.sidebar-linked').find('a').each(function(el) {
+    //         arr.push($(this).attr('href'));
+    //     });
+    //     tempSet = new Set(arr);
+    //     return Array.from(tempSet);
+    // }
 
 
     tags = parseTags();
     headline = parseHeadline();
     answerLinks = parseAnswerLinks();
-    sideLinks = parseSideLinks();
+    // sideLinks = parseSideLinks();
 
     const map = {
         'javascript': ['https://developer.mozilla.org/en-US/docs/Learn/JavaScript', 'mdn+js'],
         'java': ['https://docs.oracle.com/javase/8/', 'oracle java'],
         'php': ['http://php.net/manual/en/', 'php manual'],
         'android': ['https://developer.android.com/guide/index.html', 'android developer'],
-        'nodejs': ['https://nodejs.org/api/', 'nodejs'],
+        'nodejs': ['https://nodejs.org/api/', 'node documentation'],
         'ajax': ['https://developer.mozilla.org/en-US/docs/AJAX', 'mdn+ajax'],
         'css': ['https://developer.mozilla.org/en-US/docs/Web/CSS', 'mdn+css'],
         'jquery': ['http://api.jquery.com/', 'jquery'],
@@ -144,9 +146,11 @@ $(document).ready( () => {
     });
     });
     //logic for answer links
+    $('#dm-container').append('<div class = "dm-tag" id="dm-best-answer"><h3><a href="#">links from best answer</a></h3><div>'); 
     if(answerLinks[0]){
         answerLinks.forEach((link, index) => {
-            $('#dm-container').append('<div class = "dm-tag" id = "answer-link' + (index + 1) + '"> <a>' + 'link from best answer ' + (index+1) + '</a><div>')
+            // $('#dm-container').append('<div class = "dm-tag" id = "answer-link' + (index + 1) + '"> <a>' + 'link from best answer ' + (index+1) + '</a><div>');
+            $('#dm-container').append('<div class = "dm-tag" id = "answer-link' + (index + 1) + '"> <a>' + answerLinksText[index] + '</a><div>');            
             let idLink = '#' + 'answer-link' + (index + 1) + ' a';
             //if(link.substring(0,3) !== 'htt') link = 'http:' + link;
             $(idLink).attr('href', link).attr('target','_blank');
@@ -157,6 +161,6 @@ $(document).ready( () => {
     console.log('tags: ', tags);
     console.log('headline: ', headline);
     console.log('answer links: ', answerLinks);
-    console.log('side links: ', sideLinks);
+    // console.log('side links: ', sideLinks);
 
 });
